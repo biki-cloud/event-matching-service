38855d8446c44a4e485516feb5669fc99dbcdcf9
hibiki <hibiki123123@gmail.com>
2024-05-12 07:58:33 +0900

    good

--- main/static/main/js/script.js
+++ main/static/main/js/script.js
@@ -1,8 +1,16 @@
 console.log("xxxx");
 
+document.addEventListener('DOMContentLoaded', function() {
+  const sidebar = document.querySelector('#sidebar');
+  const storedSidebarState = localStorage.getItem('sidebarExpanded');
+  
+  if (storedSidebarState === 'true') {
+    sidebar.classList.add('expand');
+  }
 
-const hamBurger = document.querySelector(".toggle-btn");
-
-hamBurger.addEventListener("click", function () {
-  document.querySelector("#sidebar").classList.toggle("expand");
+  const hamBurger = document.querySelector(".toggle-btn");
+  hamBurger.addEventListener("click", function() {
+    sidebar.classList.toggle("expand");
+    localStorage.setItem('sidebarExpanded', sidebar.classList.contains('expand'));
+  });
 });
