fb34154bee2b8403ae8dedd90989499ba164d794
hibiki <hibiki123123@gmail.com>
2024-05-25 07:03:29 +0900

    add e2e test 2222222 333222

---
+++ static/js/left-sidebar.js
@@ -0,0 +1,16 @@
+console.log("left-sidebar.js loaded");
+
+document.addEventListener('DOMContentLoaded', function() {
+  const sidebar = document.querySelector('#sidebar');
+  const storedSidebarState = localStorage.getItem('sidebarExpanded');
+
+  if (storedSidebarState === 'true') {
+    sidebar.classList.add('expand');
+  }
+
+  const hamBurger = document.querySelector(".toggle-btn");
+  hamBurger.addEventListener("click", function() {
+    sidebar.classList.toggle("expand");
+    localStorage.setItem('sidebarExpanded', sidebar.classList.contains('expand'));
+  });
+});
