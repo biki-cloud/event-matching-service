a90dfe8fd3819049d5cd80772fae48a7351e719f
hibiki <hibiki123123@gmail.com>
2024-05-27 18:11:25 +0900

    add

--- scripts/reset_db.sh
+++ scripts/reset_db.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-source .env
+# source .env
 
 echo "DJANGO_SUPERUSER_EMAIL: $DJANGO_SUPERUSER_EMAIL"
 echo "DJANGO_SUPERUSER_PASSWORD: $DJANGO_SUPERUSER_PASSWORD"
 echo "HOST_NAME: $HOST_NAME"
 
 # Reset the database
-rm accounts/migrations/0*.py
-rm events/migrations/0*.py
+# rm accounts/migrations/0*.py
+# rm events/migrations/0*.py
 
 rm db.sqlite3
 
@@ -23,4 +23,4 @@ export DJANGO_SUPERUSER_PASSWORD=$(printenv DJANGO_SUPERUSER_PASSWORD)
 python manage.py createsuperuser --noinput
 
 # Create regular user with OrganizerProfile
-python manage.py shell < ./scripts/init_db.py
+# python manage.py shell < ./scripts/init_db.py
